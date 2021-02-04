73e11fa1b9037464d65f57027ac46b4516dc3372
Sebastian Gembis <sgembis@hawk.iit.edu>
2021-02-04 16:10:11 -0600

    lab1done

---
+++ lab01.py
@@ -0,0 +1,195 @@
+import unittest
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+#################################################################################
+# TESTING OUTPUTS
+#################################################################################
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+#################################################################################
+# EXERCISE 1
+#################################################################################
+
+# implement this function
+def is_perfect(n):
+    sum = 0
+    for x in range(n):
+        while x > 0:
+            if n % x == 0:
+                sum+=x
+            break
+    if n == sum:
+        return True
+    else:
+        return False
+
+
+# (3 points)
+def test1():
+    tc = unittest.TestCase()
+    for n in (6, 28, 496):
+        tc.assertTrue(is_perfect(n), '{} should be perfect'.format(n))
+    for n in (1, 2, 3, 4, 5, 10, 20):
+        tc.assertFalse(is_perfect(n), '{} should not be perfect'.format(n))
+    for n in range(30, 450):
+        tc.assertFalse(is_perfect(n), '{} should not be perfect'.format(n))
+
+#################################################################################
+# EXERCISE 2
+#################################################################################
+
+# implement this function
+def multiples_of_3_and_5(n):
+    sum = 0
+    for x in range(0,n):
+        if x % 3 == 0:
+            sum += x
+        elif x % 5 == 0:
+            sum += x
+    return sum
+    
+        
+
+# (3 points)
+def test2():
+    tc = unittest.TestCase()
+    tc.assertEqual(multiples_of_3_and_5(10), 23)
+    tc.assertEqual(multiples_of_3_and_5(500), 57918)
+    tc.assertEqual(multiples_of_3_and_5(1000), 233168)
+
+#################################################################################
+# EXERCISE 3
+#################################################################################
+def integer_right_triangles(p):
+    num = 0
+    for a in range(p):
+        for b in range(p):
+            for c in range(p):
+                if c > b > a:
+                    if a*a + b*b == c*c:     
+                        if a+b+c == p:
+                            num+=1
+    return num
+                
+
+def test3():
+    tc = unittest.TestCase()
+    tc.assertEqual(integer_right_triangles(60), 2)
+    tc.assertEqual(integer_right_triangles(100), 0)
+    tc.assertEqual(integer_right_triangles(180), 3)
+
+#################################################################################
+# EXERCISE 4
+#################################################################################
+
+# implement this function
+def gen_pattern(chars):
+    answer = ''
+    length = len(chars)
+    first = ''
+    second = ''
+    for x in range(length):
+        first = ('.'.join(chars[:-x-2:-1] + chars[length-x:]).center(length*4-3, '.'))
+        answer+= first+'\n'
+    for x in range(length-2,-1,-1):
+        second = ('.'.join(chars[:-x-2:-1] + chars[length-x:]).center(length*4-3,'.'))
+        answer+= second+'\n'
+    print(answer)
+    return answer
+
+def test4():
+    tc = unittest.TestCase()
+    with captured_output() as (out,err):
+        gen_pattern('@')
+        tc.assertEqual(out.getvalue().strip(), '@')
+    with captured_output() as (out,err):
+        gen_pattern('@%')
+        tc.assertEqual(out.getvalue().strip(),
+        """
+..%..
+%.@.%
+..%..
+""".strip())
+    with captured_output() as (out,err):
+        gen_pattern('ABC')
+        tc.assertEqual(out.getvalue().strip(),
+        """
+....C....
+..C.B.C..
+C.B.A.B.C
+..C.B.C..
+....C....
+""".strip())
+    with captured_output() as (out,err):
+        gen_pattern('#####')
+        tc.assertEqual(out.getvalue().strip(),
+                             """
+........#........
+......#.#.#......
+....#.#.#.#.#....
+..#.#.#.#.#.#.#..
+#.#.#.#.#.#.#.#.#
+..#.#.#.#.#.#.#..
+....#.#.#.#.#....
+......#.#.#......
+........#........
+""".strip())
+    with captured_output() as (out,err):
+        gen_pattern('abcdefghijklmnop')
+        tc.assertEqual(out.getvalue().strip(),
+"""
+..............................p..............................
+............................p.o.p............................
+..........................p.o.n.o.p..........................
+........................p.o.n.m.n.o.p........................
+......................p.o.n.m.l.m.n.o.p......................
+....................p.o.n.m.l.k.l.m.n.o.p....................
+..................p.o.n.m.l.k.j.k.l.m.n.o.p..................
+................p.o.n.m.l.k.j.i.j.k.l.m.n.o.p................
+..............p.o.n.m.l.k.j.i.h.i.j.k.l.m.n.o.p..............
+............p.o.n.m.l.k.j.i.h.g.h.i.j.k.l.m.n.o.p............
+..........p.o.n.m.l.k.j.i.h.g.f.g.h.i.j.k.l.m.n.o.p..........
+........p.o.n.m.l.k.j.i.h.g.f.e.f.g.h.i.j.k.l.m.n.o.p........
+......p.o.n.m.l.k.j.i.h.g.f.e.d.e.f.g.h.i.j.k.l.m.n.o.p......
+....p.o.n.m.l.k.j.i.h.g.f.e.d.c.d.e.f.g.h.i.j.k.l.m.n.o.p....
+..p.o.n.m.l.k.j.i.h.g.f.e.d.c.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p..
+p.o.n.m.l.k.j.i.h.g.f.e.d.c.b.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p
+..p.o.n.m.l.k.j.i.h.g.f.e.d.c.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p..
+....p.o.n.m.l.k.j.i.h.g.f.e.d.c.d.e.f.g.h.i.j.k.l.m.n.o.p....
+......p.o.n.m.l.k.j.i.h.g.f.e.d.e.f.g.h.i.j.k.l.m.n.o.p......
+........p.o.n.m.l.k.j.i.h.g.f.e.f.g.h.i.j.k.l.m.n.o.p........
+..........p.o.n.m.l.k.j.i.h.g.f.g.h.i.j.k.l.m.n.o.p..........
+............p.o.n.m.l.k.j.i.h.g.h.i.j.k.l.m.n.o.p............
+..............p.o.n.m.l.k.j.i.h.i.j.k.l.m.n.o.p..............
+................p.o.n.m.l.k.j.i.j.k.l.m.n.o.p................
+..................p.o.n.m.l.k.j.k.l.m.n.o.p..................
+....................p.o.n.m.l.k.l.m.n.o.p....................
+......................p.o.n.m.l.m.n.o.p......................
+........................p.o.n.m.n.o.p........................
+..........................p.o.n.o.p..........................
+............................p.o.p............................
+..............................p..............................
+""".strip()
+)
+
+#################################################################################
+# RUN ALL TESTS
+#################################################################################
+def main():
+    test1()
+    test2()
+    test3()
+    test4()
+
+if __name__ == '__main__':
+    main()
